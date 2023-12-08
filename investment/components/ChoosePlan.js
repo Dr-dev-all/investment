7d22a96b4e531e2e9536fbf66870190d07396f5a
Dr-dev-all <onyemachristian95@gmail.com>
2023-12-08 10:33:24 +0000

    ADMIN 2

--- investment/components/ChoosePlan.js
+++ investment/components/ChoosePlan.js
@@ -2,7 +2,16 @@ export default function ChoosePlan() {
   const content = (
     <section>
       <h1>Make a deposit</h1>
-      <div></div>
+      <div>
+        <h2>Choose a plan</h2>
+        <div className="plan-container">
+          <div className="plan">
+            <h3>Basic</h3>
+            <p>$10/month</p>
+            <p>10GB storage</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 
