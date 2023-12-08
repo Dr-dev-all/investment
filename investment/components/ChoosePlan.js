4d0a03496f2822e9918ff1087b125bf0bc41aa35
Dr-dev-all <onyemachristian95@gmail.com>
2023-12-08 10:33:09 +0000

    admin

--- investment/components/ChoosePlan.js
+++ investment/components/ChoosePlan.js
@@ -1,7 +1,8 @@
 export default function ChoosePlan() {
   const content = (
     <section>
-      <h1>choose plan</h1>
+      <h1>Make a deposit</h1>
+      <div></div>
     </section>
   );
 
--- investment/components/Withdraw.js
+++ investment/components/Withdraw.js
@@ -1,7 +1,16 @@
 export default function Withdraw() {
   const content = (
     <section>
-      <h1>withdraw page</h1>
+      <h1>ASK FOR WITHDRAWal</h1>
+      <form>
+        <label>Amount:</label>
+        <input></input>
+
+        <label>Wallet Address:</label>
+        <input></input>
+
+        <button>Send to wallet</button>
+      </form>
     </section>
   );
 
