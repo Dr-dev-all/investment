fac2c9e45323618b8c0927b733d8ae03d32197ec
brownzeb <uzebebrown@gmail.com>
2023-12-25 16:13:40 +0100

    middleware

--- investment/middleware.ts
+++ investment/middleware.ts
@@ -1,11 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
 
 export function middleware(req: NextRequest) {
-  const accessToken = req.headers.get("authorization")?.split(" ")[1];
+  // const accessToken = req.headers.get("authorization")?.split(" ")[1];
 
-  console.log(accessToken);
+  // const cookieStore = cookies();
+  // const data = cookieStore.get("accessToken");
+
+  // console.log(data);
+
+  const token = req.cookies.get("jwt")?.value || "";
+
+  console.log(token);
+
+  // return NextResponse.redirect(new URL("/login", req.url));
 }
 
-export const config = {
-  matcher: ["/login/userdash", "/login/adminDash"],
-};
+// export const config = {
+//   matcher: ["/login/userdash", "/login/adminDash", "/register"],
+// };
