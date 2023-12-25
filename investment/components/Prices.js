b72302e1f1003c5fbef82656c99330ecb1a09e13
brownzeb <uzebebrown@gmail.com>
2023-12-25 10:04:33 +0100

    latest

--- investment/components/Prices.js
+++ investment/components/Prices.js
@@ -3,9 +3,27 @@ import Link from "next/link";
 
 export default function () {
   const starterPlan = [
-    { name: "Interest", value: "128.8%", id: 1 },
+    { name: "Interest", value: "20%", id: 1 },
     { name: "Investment", value: "$500-$5000", id: 2 },
-    { name: "Capital Bank", value: "No", id: 3 },
+    { name: "Capital Back", value: "Yes", id: 3 },
+    { name: "Return Type", value: "Period", id: 4 },
+    { name: "Number of Period", value: "5 Times", id: 5 },
+    { name: "Profit Withdraw", value: "Anytime", id: 6 },
+  ];
+
+  const premium = [
+    { name: "Interest", value: "130.1%", id: 1 },
+    { name: "Investment", value: "$5000-$10000", id: 2 },
+    { name: "Capital Back", value: "Yes", id: 3 },
+    { name: "Return Type", value: "Period", id: 4 },
+    { name: "Number of Period", value: "7 Times", id: 5 },
+    { name: "Profit Withdraw", value: "Anytime", id: 6 },
+  ];
+
+  const gold = [
+    { name: "Interest", value: "180%", id: 1 },
+    { name: "Investment", value: "$10000-$50000", id: 2 },
+    { name: "Capital Bank", value: "Yes", id: 3 },
     { name: "Return Type", value: "Period", id: 4 },
     { name: "Number of Period", value: "7 Times", id: 5 },
     { name: "Profit Withdraw", value: "Anytime", id: 6 },
@@ -36,50 +54,59 @@ export default function () {
         </div>
       </section>
 
-      <section className="mx-auto mt-8 center-with-flex lg:px-8 ">
+      <article className="article-style  mb-5  ">
         {/* bf arrow */}
-        <div className="center-with-grid md:grid-cols-3 w-screen">
-          <div className=" center-with-flex  plan-style">
-            <h1>plan 1</h1>
-            <ul className="w-[90%] h-[80%]  mx-auto block">
+        {/* <h1 className="underline mb-4 underline-offset-8 text-lg font-semibold leading-6 text-gray-900">
+          Our Best Plan
+        </h1> */}
+        <div className="2xl:mx-18 md:px-8 lg:px-8 center-with-grid md:grid-cols-3 w-screen">
+          <div className="center-with-flex  plan-style">
+            <h1 className=" underline font-black tracking-wider decoration-2 underline-offset-2">
+              Starter plan
+            </h1>
+            <ul className="w-[92%] h-[98%]  mx-auto block">
               {starterPlan.map((data) => (
                 <li key={data.id} className="plan-items">
-                  {data.name} <span className="inline  ">{data.value}</span>
+                  {data.name}: <span className="inline  ">{data.value}</span>
                 </li>
               ))}
             </ul>
-            <Link href="/plan-1" className="plan-link-style">
+            <Link href="/prices" className="plan-link-style">
+              CHOOSE PLAN <GoArrowUpRight className="inline" />
+            </Link>
+          </div>
+          <div className=" center-with-flex plan-style">
+            <h1 className=" underline font-black tracking-wider decoration-2 underline-offset-2">
+              Premium
+            </h1>
+            <ul className="w-[92%] h-[98%]  mx-auto block">
+              {premium.map((data) => (
+                <li key={data.id} className="plan-items">
+                  {data.name}: <span className="inline  ">{data.value}</span>
+                </li>
+              ))}
+            </ul>
+            <Link href="/prices" className="plan-link-style">
               CHOOSE PLAN <GoArrowUpRight className="inline" />
             </Link>
           </div>
           <div className="center-with-flex plan-style">
-            <h1>plan 2</h1>
-            <ul className="w-[80%] h-[80%]  mx-auto block">
-              {starterPlan.map((data) => (
+            <h1 className=" underline font-black tracking-wider decoration-2 underline-offset-2">
+              Gold
+            </h1>
+            <ul className="w-[92%] h-[98%]  mx-auto block">
+              {gold.map((data) => (
                 <li key={data.id} className="plan-items">
-                  {data.name} <span className="inline  ">{data.value}</span>
+                  {data.name}: <span className="inline  ">{data.value}</span>
                 </li>
               ))}
             </ul>
-            <Link href="/plan-1" className="plan-link-style">
-              CHOOSE PLAN <GoArrowUpRight className="inline" />
-            </Link>
-          </div>
-          <div className="center-with-flex plan-style">
-            <h1>plan 3</h1>
-            <ul className="w-[80%] h-[80%]  mx-auto block">
-              {starterPlan.map((data) => (
-                <li key={data.id} className="plan-items">
-                  {data.name} <span className="inline  ">{data.value}</span>
-                </li>
-              ))}
-            </ul>
-            <Link href="/plan-1" className="plan-link-style">
+            <Link href="/prices" className="plan-link-style">
               CHOOSE PLAN <GoArrowUpRight className="inline" />
             </Link>
           </div>
         </div>
-      </section>
+      </article>
 
       <section className="mx-auto max-w-7xl px-6 lg:px-8 mt-14 sm:mt-12 lg:mt-20">
         <div className="mx-auto max-w-2xl lg:max-w-none">
@@ -90,7 +117,9 @@ export default function () {
                 comprehensive financial management experience, including
                 personalized advice and strategic planning.
               </p>
-              <button className="plan-link-style">Sign Up Now</button>
+              <button className="plan-link-style">
+                <a href="register">Sign Up Now</a>
+              </button>
             </div>
           </div>
         </div>
@@ -142,7 +171,9 @@ export default function () {
                 choosing the right one for you? Our support team is ready to
                 help. Contact us here.
               </p>
-              <button className="plan-link-style">Contact</button>
+              <button className="plan-link-style">
+                <a href="contact">Contact</a>
+              </button>
             </div>
           </div>
         </div>
@@ -163,7 +194,9 @@ export default function () {
                 plan, sign up today, and embark on your investment journey with
                 BullHarvest.
               </p>
-              <button className="plan-link-style">Get Started Now</button>
+              <button className="plan-link-style">
+                <a href="register">Get Started Now</a>
+              </button>
             </div>
           </div>
         </div>
