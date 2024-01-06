cf70417be54be754b88588f14a35b0e6c584ce80
wizardlord956 <108439096+wizardlord956@users.noreply.github.com>
2024-01-06 06:49:47 -0500

    indejs file deleted with cleard cache

--- investment/components/index.ts
+++ investment/components/index.ts
@@ -1,6 +1,23 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import UserdashboardHeader from "./UserdashboardHeader";
-import UserdashboardFooter from "./UserdashboardFooter";
+import Header from './Header';
+import Footer from './Footer';
+import UserdashboardHeader from './UserdashboardHeader';
+import UserdashboardFooter from './UserdashboardFooter';
+import Landing from './Landing';
+import Login from './Login';
+import UserDashBoard from './Userdash';
+import VerifyPassword from './Verifypassword';
+import About from './About';
+import Contact from './Contact';
 
-export { Header, Footer, UserdashboardHeader, UserdashboardFooter };
+export {
+  Header,
+  Footer,
+  UserdashboardHeader,
+  UserdashboardFooter,
+  Landing,
+  UserDashBoard,
+  Login,
+  VerifyPassword,
+  About,
+  Contact,
+};
