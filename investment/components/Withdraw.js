6896b8dee3ac79a189af9e3f6dfb09c7072d6bd9
wizardlord956 <108439096+wizardlord956@users.noreply.github.com>
2024-01-06 05:28:47 -0500

    removed server folder

--- investment/components/Withdraw.js
+++ investment/components/Withdraw.js
@@ -1,13 +1,12 @@
-"use client";
-import { useForm } from "react-hook-form";
-import useAxiosPrivate from "@/hooks/useAxiosPrivate";
-import { User } from "../../server/models/userModels";
-import { useState } from "react";
+'use client';
+import { useForm } from 'react-hook-form';
+import useAxiosPrivate from '@/hooks/useAxiosPrivate';
+import { useState } from 'react';
 
 export default function Withdraw() {
   const axiosPrivate = useAxiosPrivate();
 
-  const [appError, setAppError] = useState("");
+  const [appError, setAppError] = useState('');
 
   const {
     register,
@@ -27,15 +26,15 @@ export default function Withdraw() {
     // getting user data via form
     for (let [key, value] of formData) {
       if (key && value) {
-        if (key === "amount") {
+        if (key === 'amount') {
           userData.amount = value;
         }
 
-        if (key === "wallet") {
+        if (key === 'wallet') {
           userData.wallet = value;
         }
 
-        if (key === "wallet_type") {
+        if (key === 'wallet_type') {
           userData.walletType = value;
         }
       }
@@ -44,17 +43,17 @@ export default function Withdraw() {
     // console.log(userData);
 
     try {
-      const response = await axiosPrivate("/api/users/withdraw", {
-        method: "POST",
+      const response = await axiosPrivate('/api/users/withdraw', {
+        method: 'POST',
         data: JSON.stringify(userData),
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
       });
       const serverData = await response.data;
       // console.log("passing to the database");
       // console.log(serverData);
     } catch (error) {
       // console.log(error);
-      setAppError("Network error...., try again later");
+      setAppError('Network error...., try again later');
     }
   };
 
@@ -66,7 +65,7 @@ export default function Withdraw() {
       <form className="mt-9 max-w-md mx-auto" onSubmit={onSubmit}>
         <label className="text-base " htmlFor="amount">
           Amount:
-        </label>{" "}
+        </label>{' '}
         <br />
         <input
           // {...register("amount", {
@@ -79,8 +78,7 @@ export default function Withdraw() {
           name="amount"
           id="amount"
           className="block text-black  py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder="$50.00"
-        ></input>
+          placeholder="$50.00"></input>
         <br />
         <div>
           <select
@@ -88,8 +86,7 @@ export default function Withdraw() {
             name="wallet_type"
             type="text"
             id="wallet_type"
-            className="mb-2 w-full  border p-2 border-gray-300 rounded-md focus:outline-none focus-border-blue-500"
-          >
+            className="mb-2 w-full  border p-2 border-gray-300 rounded-md focus:outline-none focus-border-blue-500">
             <option value="bitcoin">BTC</option>
             <option value="ethereum">ETH</option>
             <option value="litecoin">USDT</option>
@@ -102,13 +99,11 @@ export default function Withdraw() {
           id="wallet"
           name="wallet"
           // {...register("wallet")}
-          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        ></input>
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"></input>
         <br />
         <button
           type="submit"
-          className="py-2.5  px-5 me-2 mb-2 text-sm font-semibold font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-        >
+          className="py-2.5  px-5 me-2 mb-2 text-sm font-semibold font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
           Withdraw
         </button>
       </form>
