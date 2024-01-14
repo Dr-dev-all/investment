469a78eb61cf72d070acecc31b2a8ae502eba3df
brown uzebe <uzebebrown@gmail.com>
2024-01-14 18:38:46 +0100

    new commit made

--- investment/components/Withdraw.js
+++ investment/components/Withdraw.js
@@ -1,58 +1,46 @@
 'use client';
 import { useForm } from 'react-hook-form';
 import useAxiosPrivate from '@/hooks/useAxiosPrivate';
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import PuffLoader from 'react-spinners/PuffLoader';
-// import useAxiosPrivate from "@/hooks/useAxiosPrivate";
 
 export default function Withdraw() {
   const axiosPrivate = useAxiosPrivate();
 
   const [appError, setAppError] = useState('');
-  const [user, setUser] = useState({});
   const [formLoading, setFormLoading] = useState(false);
+  const [dataReset, setDataReset] = useState('');
   const [walletError, setWalletError] = useState(false);
   const [amountError, setAmountError] = useState(false);
-  // testing my git data
+  const [serverError, setServerError] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
+  // const inputRef = useRef(null);
 
-  useEffect(() => {
-    let isMounted = true;
-
-    const controller = new AbortController();
-
-    const getUser = async () => {
-      try {
-        const response = await axiosPrivate.get('/users/getsingleuser', {
-          signal: controller.signal,
-        });
-        isMounted && setUser((prev) => ({ ...prev, data: response.data }));
-        // console.log({ serverdata: data });
-      } catch (error) {
-        // console.log(error);
-        if (error) {
-          setAppError('Network error... try again later');
-        }
-      }
-    };
-
-    getUser();
-
-    return () => {
-      isMounted = false;
-      controller.abort();
-    };
-  }, [axiosPrivate]);
-
+  const checkSubmit = () => {
+    if (!formLoading || !walletError || !amountError || !serverError) {
+      setAppError('Success');
+      setSuccessMessage(true);
+    } else {
+      setSuccessMessage(false);
+    }
+  };
   // FORM SUBMISSION
   const onSubmit = async (event) => {
     event.preventDefault();
+    // console.log(data);
 
     const formData = new FormData(event.target);
-    mainData = Object.fromEntries(formData);
+    const mainData = Object.fromEntries(formData);
+
+    console.log(mainData);
 
     if (mainData.amount === '') {
       setAmountError(true);
-      return setAppError('Please enter an amount to withdraw');
+      return setAppError('Please enter an amount to withdraw.');
+    } else if (!Number(mainData.amount)) {
+      setAmountError(true);
+
+      return setAppError('Please enter a valid amount digits.');
     } else {
       setAmountError(false);
     }
@@ -71,32 +59,74 @@ export default function Withdraw() {
         data: JSON.stringify(mainData),
         headers: { 'Content-Type': 'application/json' },
       });
-      const serverData = await response.data;
+      const serverData = response.data;
+      console.log({ success: serverData });
+      // console.log("passing to the database");
     } catch (error) {
-      setAppError('');
+      console.log(error);
+      if (
+        error.response.status === 400 &&
+        error.response.data === 'invalid-wallet-address'
+      ) {
+        console.log('invalid address');
+
+        setServerError(true);
+        return setAppError(`Invalid ${mainData.walletType} address`);
+      } else {
+        setServerError(false);
+      }
+
+      if (
+        error.response.status === 400 &&
+        error.response.data === 'insufficient'
+      ) {
+        console.log('insufficient');
+        setServerError(true);
+        return setAppError(`Insufficient balance.`);
+      } else {
+        setServerError(false);
+      }
+
+      setAmountError('');
     } finally {
+      // reset();
       event.target.reset();
       setFormLoading(false);
     }
   };
 
   const content = (
-    <section className='mt-[8.5rem] px-5  '>
-      <h1 className='text-3xl text-center font-semibold dark:text-white mx-5 my-3'>
-        ASK FOR WITHDRAWAL
+    <section className='mt-[9rem] px-5  '>
+      <h1 className='text-3xl text-center text-black  font-semibold mx-5 my-3'>
+        WITHDRAWAL
       </h1>
+      {serverError && appError && (
+        <p className='form-error-style'>{appError}</p>
+      )}
+
+      {successMessage &&
+        !serverError &&
+        appError &&
+        !amountError &&
+        !walletError && (
+          <p className='text-green-500 text-[1.2rem]'>{appError}</p>
+        )}
+
       {formLoading ? (
         <PuffLoader />
       ) : (
-        <form className='mt-9 max-w-md mx-auto' onSubmit={onSubmit}>
+        <form
+          id='submitForm'
+          className='mt-9 max-w-md mx-auto'
+          onSubmit={onSubmit}>
           <label className='text-base ' htmlFor='amount'>
             Amount:
           </label>{' '}
           <br />
           <input
             type='text'
-            id='amount'
             name='amount'
+            id='amount'
             className='block text-black  py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'
             placeholder='$50.00'></input>
           {amountError && appError && (
@@ -108,6 +138,7 @@ export default function Withdraw() {
               type='text'
               name='walletType'
               id='walletType'
+              // {...register('walletType')}
               className='mb-2 w-full  border p-2 border-gray-300 rounded-md focus:outline-none focus-border-blue-500'>
               <option value='bitcoin'>BTC</option>
               <option value='ethereum'>ETH</option>
@@ -118,6 +149,7 @@ export default function Withdraw() {
           <br />
           <input
             type='text'
+            // ref={formRef}
             id='wallet'
             name='wallet'
             className='block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-dark dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer'></input>
@@ -126,6 +158,9 @@ export default function Withdraw() {
           )}
           <br />
           <button
+            onClick={() => {
+              checkSubmit();
+            }}
             type='submit'
             className='py-2.5  px-5 me-2 mb-2 text-sm font-semibold font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700'>
             Withdraw
